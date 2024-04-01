23a9a5aa6a0262bed573951eb8968be388cbcaef
Lauritz Leifermann <lauritz@qours.es>
2024-04-01 15:43:42 +0200

    add single shade darker than white

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -10,7 +10,7 @@ export default function Hero() {
     // const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
     return (
-        <div className="bg-white">
+        <div className="bg-[#FEFEFE]">
             <header className="absolute inset-x-0 top-0 z-50">
                 <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
                     <div className="flex lg:flex-1">
