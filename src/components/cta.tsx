00ec0ab586b852213974023ee15876bcf77bb989
prevery <164774401+prevery@users.noreply.github.com>
2024-04-07 11:29:02 +0200

    Update cta.tsx

--- src/components/cta.tsx
+++ src/components/cta.tsx
@@ -14,7 +14,7 @@ export default function CTA() {
                             href="mailto:testkunde@prevery.de"
                             className="bezel rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
                         >
-                            Testkund:in werden
+                            Testkunde/in werden
                         </a>
                         <a href="mailto:kooperationspartner@prevery.de" className="text-sm font-semibold leading-6 text-white ring-white ring-1 p-2 rounded-md">
                             Kooperationspartner:in werden
